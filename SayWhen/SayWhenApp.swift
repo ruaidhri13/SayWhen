a283f4cb038dee0f967bb481ab0552c10e36f2e6
Ruaidhri O'Neill <Ruaidhri2006@gmail.com>
2021-08-14 15:47:43 +0100

    Initial Commit

---
+++ SayWhen/SayWhenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayWhenApp.swift
+//  SayWhen
+//
+//  Created by Ruaidhrí O'Neill on 14/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SayWhenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
